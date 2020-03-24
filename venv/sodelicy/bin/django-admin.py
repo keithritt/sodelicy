49708acceeb5c335da8d688edc58e8d9f1b871bd
keith.ritter <keith.ritter@inmar.com>
2020-03-24 00:16:50 -0400

    created virtual environment

---
+++ venv/sodelicy/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kritter/repos/sodelicy/venv/sodelicy/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv/sodelicy/lib/python3.7/_bootlocale.py
@@ -0,0 +1,1 @@
+/home/kritter/.pyenv/versions/3.7.5/lib/python3.7/_bootlocale.py
